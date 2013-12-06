c4104161a5ff678d908f6dea444b2b4039994eb9
sdawara <siddharthdawara@gmail.com>
2013-12-06 15:20:39 +0000

    quotes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 require 'bundler/capistrano'
 
-set :application, "cruisecontrol.rb"
-set :repository,  "git@github.com:uswitch/cruisecontrol.rb.git"
-set :user,        "deploy"
+set :application, 'cruisecontrol.rb'
+set :repository,  'git@github.com:uswitch/cruisecontrol.rb.git'
+set :user,        'deploy'
 
 set :deploy_via, :remote_cache
 set :deploy_to, "/var/www/#{application}"
@@ -14,7 +14,7 @@ set :scm, :git
 set :use_sudo, false
 set :ssh_options, {:port => 8008, :forward_agent => true}
 
-role :web, "ec2-54-228-156-149.eu-west-1.compute.amazonaws.com"
+role :web, 'ec2-54-228-156-149.eu-west-1.compute.amazonaws.com'
 
 
 namespace :deploy do
@@ -23,22 +23,22 @@ namespace :deploy do
   
 
   task :install_bundler do
-    sudo "gem install bundler"
+    sudo 'gem install bundler'
   end
   
   task :restart do
-    sudo "stop switching-reporting || echo 0"
-    sudo "start switching-reporting || echo 0"
+    sudo 'stop switching-reporting || echo 0'
+    sudo 'start switching-reporting || echo 0'
   end
   
   task :restart_nginx do
-    sudo "/etc/init.d/nginx restart"
+    sudo '/etc/init.d/nginx restart'
   end
   
   task :install_upstart do
     sudo "cp #{current_path}/config/cruisecontrol.conf /etc/init/cruisecontrol.conf || echo 0"
   end
    
-  after "deploy:setup", "deploy:install_bundler"
-  after "deploy", "deploy:install_upstart", "deploy:cleanup"
+  after 'deploy:setup', 'deploy:install_bundler'
+  after 'deploy', 'deploy:install_upstart', 'deploy:cleanup'
 end
